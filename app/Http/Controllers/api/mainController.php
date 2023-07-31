cbf36c41d0711421ff354825a8afb38da0fa7c77
ahmadnial <ahmadhky@gmail.com>
2023-07-31 14:45:31 +0700

    add tanggal masuk dari SEP terbit

--- app/Http/Controllers/api/mainController.php
+++ app/Http/Controllers/api/mainController.php
@@ -17,17 +17,17 @@ class mainController extends Controller
     public function index()
     {
         try {
-            $listAll = DB::select(" SELECT d.fs_nm_pasien, d.fs_mr, h.fs_no_sep, h.fs_no_peserta, g.fs_nm_layanan, f.fs_nm_bed, e.fs_nm_kelas, b.fd_tgl_in , sum(a.fn_total) fn_count_all, b.fs_kd_reg 
-                        from TA_TRS_BILLING a
-                        left join ta_trs_bed b on a.fs_kd_reg = b.fs_kd_reg
-                        left join ta_registrasi c on a.fs_kd_reg = c.fs_kd_reg
-                        left join tc_mr d on c.fs_mr = d.fs_mr
-                        left join ta_kelas e on b.fs_kd_kelas = e.fs_kd_kelas
-                        left join ta_bed f on b.fs_kd_bed = f.fs_kd_bed
-                        left join ta_layanan g on b.fs_kd_layanan = g.fs_kd_layanan
-                        left join VCLAIM_SEP h on c.fs_kd_reg = h.fs_kd_reg
-                        where b.fd_tgl_out='3000-01-01' and b.fd_tgl_void='3000-01-01' and c.fs_kd_tipe_jaminan in ('NPB01','PBI01') group by d.fs_nm_pasien, d.fs_mr, h.fs_no_sep, h.fs_no_peserta, g.fs_nm_layanan,
-                        f.fs_nm_bed, e.fs_nm_kelas, b.fd_tgl_in, b.fs_kd_reg ");
+            $listAll = DB::select(" SELECT d.fs_nm_pasien, d.fs_mr, h.fs_no_sep, h.fd_tgl_trs, h.fs_no_peserta, g.fs_nm_layanan, f.fs_nm_bed, e.fs_nm_kelas, b.fd_tgl_in , sum(a.fn_total) fn_count_all, b.fs_kd_reg 
+                            from TA_TRS_BILLING a
+                            left join ta_trs_bed b on a.fs_kd_reg = b.fs_kd_reg
+                            left join ta_registrasi c on a.fs_kd_reg = c.fs_kd_reg
+                            left join tc_mr d on c.fs_mr = d.fs_mr
+                            left join ta_kelas e on b.fs_kd_kelas = e.fs_kd_kelas
+                            left join ta_bed f on b.fs_kd_bed = f.fs_kd_bed
+                            left join ta_layanan g on b.fs_kd_layanan = g.fs_kd_layanan
+                            left join VCLAIM_SEP h on c.fs_kd_reg = h.fs_kd_reg
+                            where b.fd_tgl_out='3000-01-01' and b.fd_tgl_void='3000-01-01' and c.fs_kd_tipe_jaminan in ('NPB01','PBI01') group by d.fs_nm_pasien, d.fs_mr, h.fs_no_sep, h.fd_tgl_trs, h.fs_no_peserta, g.fs_nm_layanan,
+                            f.fs_nm_bed, e.fs_nm_kelas, b.fd_tgl_in, b.fs_kd_reg ");
             return response()->json($listAll, Response::HTTP_OK);
         } catch (QueryException $e) {
             $error = [
